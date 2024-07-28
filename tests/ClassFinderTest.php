4533abb3c3b89d4c9519e3c7450c3b58cf281214
Enea Dhack <enea.so@live.com>
2024-07-27 19:45:19 -0500

    fix: relative path

--- tests/ClassFinderTest.php
+++ tests/ClassFinderTest.php
@@ -16,13 +16,14 @@ final class ClassFinderTest extends TestCase
     #[Test]
     public function find_tagged_classes_as_entities(): void
     {
-        $finder  = new ClassFinder(self::basePath('App'), 'Vaened\\TagFinder\\Tests\\App');
-        $classes = $finder->instancesOf(Handler::class);
+        $basePath = self::basePath('App');
+        $finder   = new ClassFinder($basePath, 'Vaened\\TagFinder\\Tests\\App');
+        $classes  = $finder->instancesOf(Handler::class);
 
         $this->assertCount(2, $classes);
         $this->assertSame([
-            "/app/tests/App/Products/CreateProductHandler.php"    => "Vaened\TagFinder\Tests\App\Products\CreateProductHandler",
-            "/app/tests/App/Categories/CreateCategoryHandler.php" => "Vaened\TagFinder\Tests\App\Categories\CreateCategoryHandler",
+            "$basePath/Products/CreateProductHandler.php"    => "Vaened\TagFinder\Tests\App\Products\CreateProductHandler",
+            "$basePath/Categories/CreateCategoryHandler.php" => "Vaened\TagFinder\Tests\App\Categories\CreateCategoryHandler",
         ], $classes);
     }
 }
